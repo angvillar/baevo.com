09ce55b30ac8f9897155d25d9645483a502cca36
angvillar <angvillar@gmail.com>
2018-05-24 13:57:57 +0200

    added acf

--- wp-content/themes/baevo/functions.php
+++ wp-content/themes/baevo/functions.php
@@ -56,3 +56,9 @@ class StarterSite extends TimberSite {
 }
 
 new StarterSite();
+
+/**
+ * Enable ACF 5 early access
+ * Requires at least version ACF 4.4.12 to work
+ */
+define('ACF_EARLY_ACCESS', '5');
